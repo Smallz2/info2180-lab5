d7953a8f5ae9f594acc7fd9a841341ded5618d6e
Dexter Small <dexterarron@gmail.com>
2020-11-19 15:35:06 -0400

    add cities lookup and cleaned up code, finished lab

--- world.js
+++ world.js
@@ -9,49 +9,50 @@ window.onload = function() {
 function main() {
 	var search_button = document.querySelector("#lookup-country");
 	var reset_button = document.querySelector("#lookup-reset");
+	var city_button = document.querySelector("#lookup-city");
 	fetchData()
 
-	search_button.addEventListener('click', fetchSearch);
-	reset_button.addEventListener('click', resetSearch);
+	search_button.addEventListener("click", function() {
+		fetchData(event, 'search_country')
+	});
+	city_button.addEventListener("click", searchCities)
+	reset_button.addEventListener("click", resetSearch)
 }
 
-function fetchData(event) {
-	try {
-		fetch('./world.php') 
-			.then(
-				function(response) {
-					if (response.status !== 200) {
-						console.log("Something went wrong. Status code: " + response.status);
-						return;
-					}
-
-					response.text().then(function(promise) {
-						updateUI(promise);
-					});
-				}
-			)
-			.catch(function(error) {
-				console.error("fetching error: " + error);
-			});
-	} catch(error) {
-		console.error(error);
+function searchCities(event) {
+	console.log("hello")
+	if (document.querySelector("#country").value == "") {
+		alert("country can't be blank");
+		return;
 	}
+
+	fetchData(event, 'search_country_city')
 }
 
-function resetSearch() {
+function resetSearch(event) {
 	document.querySelector("#country").value = '';
-	fetchData()
+	fetchData(event, '')
 }
 
-function fetchSearch(event) {
+function fetchData(event, calltype) {
 	var search_input = document.querySelector("#country").value;
-
+	
 	try {
-		// Set url params
-		var params = { country: `${search_input}` }
-		var url_params = new URLSearchParams(Object.entries(params));
+		var fetch_url = './world.php';
+		var params = "";
+		var url_params = "";
+	
+		if (calltype == "search_country") {
+			params = { country: `${search_input}` }
+			url_params = new URLSearchParams(Object.entries(params));
+			fetch_url = './world.php?' + url_params;
+		} else if (calltype == "search_country_city") {
+			params = { country: `${search_input}`, context: 'cities' }
+			url_params = new URLSearchParams(Object.entries(params));
+			fetch_url = './world.php?' + url_params;
+		}
 
-		fetch('./world.php?' + url_params)
+		fetch(fetch_url)
 			.then(
 				function(response) {
 					if (response.status !== 200) {
--- world.php
+++ world.php
@@ -11,28 +11,31 @@ $stmt = $conn->query("SELECT * FROM countries");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+// Get params
 $search_query = trim(htmlspecialchars(strip_tags($_GET['country'])));
-
-$matching_countries = $conn->query("SELECT * FROM countries WHERE name LIKE '%$search_query%')");
+$search_query_context = trim(htmlspecialchars(strip_tags($_GET['context'])));
 
 if ($search_query != "") {
+	$matching_countries = $conn->query("SELECT * FROM countries WHERE name LIKE '%$search_query%'");
 	$results = $matching_countries;
 }
 
+if ($search_query_context != "" and $search_query != "") {
+	$cities = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities INNER JOIN countries ON cities.country_code = countries.code WHERE countries.name = '$search_query'");
+	$results = $cities;
+}
+
 ?>
-<table class="table">
-	<thead>
-		<th>Name</th>
-		<th>Continent</th>
-		<th>Independence</th>
-		<th>Head of State</th>
-	</thead>
-	<tbody>
-		<?php if (empty($results)): ?>
-			<tr>
-				<td colspan="4">Nothing found</td>
-			</tr>
-		<?php else: ?>
+
+<?php if ($search_query == "" and $search_query_context == ""): ?>
+	<table class="table">
+		<thead>
+			<th>Name</th>
+			<th>Continent</th>
+			<th>Independence</th>
+			<th>Head of State</th>
+		</thead>
+		<tbody>
 			<?php foreach ($results as $row): ?>
 			  <tr>
 				  <td><?= $row['name'] ?></td>
@@ -41,6 +44,44 @@ if ($search_query != "") {
 				  <td><?= $row['head_of_state'] ?></td>
 				</tr>
 			<?php endforeach; ?>
-		<?php endif; ?>
-	</tbody>
-</table>
+		</tbody>
+	</table>
+<?php elseif ($search_query != "" and $search_query_context == ""): ?>
+	<table class="table">
+		<thead>
+			<th>Name</th>
+			<th>Continent</th>
+			<th>Independence</th>
+			<th>Head of State</th>
+		</thead>
+		<tbody>
+			<?php foreach ($results as $row): ?>
+			  <tr>
+				  <td><?= $row['name'] ?></td>
+				  <td><?= $row['continent'] ?></td>
+				  <td><?= $row['independence_year'] ?></td>
+				  <td><?= $row['head_of_state'] ?></td>
+				</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+<?php endif; ?>
+
+<?php if(isset($search_query_context) and $search_query_context != ""): ?>
+	<table class="table">
+		<thead>
+			<th>Name</th>
+			<th>District</th>
+			<th>Population</th>
+		</thead>
+		<tbody>
+			<?php foreach ($results as $row): ?>
+			  <tr>
+				  <td><?= $row['name'] ?></td>
+				  <td><?= $row['district'] ?></td>
+				  <td><?= $row['population'] ?></td>
+				</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+<?php endif; ?>
